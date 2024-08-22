6caa5306782102ab717f6a35771f0fb2e2bef090
Jerry Abadi <abadijerry44@gmail.com>
2024-08-22 20:04:37 +0000

    get wallet balance api integration

--- p2p-loan-app/src/app/(platform)/(dashboard)/(home)/borrower/wallet/page.tsx
+++ p2p-loan-app/src/app/(platform)/(dashboard)/(home)/borrower/wallet/page.tsx
@@ -1,25 +1,92 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardFooter, CardHeader } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { FaEye, FaEyeSlash, FaCopy } from 'react-icons/fa';
+import { FaCopy, FaEye, FaEyeSlash } from 'react-icons/fa';
 import { CircleDollarSign } from 'lucide-react';
 import BorrowerWalletTable from '@/components/borrower-components/borrower-wallet-table';
+import useWallet from '@/hooks/useWallet';
+import { toast } from 'sonner';
+import { AxiosError } from 'axios';
+import WalletService, { WalletBalance } from '@/services/walletService';
+import { useQuery } from '@tanstack/react-query';
 
 const Wallet = () => {
   const router = useRouter();
   const [isBalanceVisible, setIsBalanceVisible] = useState(true);
-  const walletId = '7151617725';
+  const [walletId, setWalletId] = useState<string | null>(null);
+  const [accountNumber, setAccountNumber] = useState<string | null>(null);
+  const { getWalletQuery } = useWallet();
+
+  useEffect(() => {
+    if (getWalletQuery.isSuccess && getWalletQuery.data) {
+      const { result } = getWalletQuery.data;
+      if (result.length > 0) {
+        const firstWallet = result[0];
+        setWalletId(firstWallet.id);
+        setAccountNumber(firstWallet.accountNumber);
+      }
+    }
+  }, [getWalletQuery.isSuccess, getWalletQuery.data]);
+
+  useEffect(() => {
+    if (getWalletQuery.isError) {
+      toast.error('Failed to fetch wallet');
+    }
+  }, [getWalletQuery.isError]);
+
+  const {
+    data: balanceData,
+    isLoading: isBalanceLoading,
+    isError: isBalanceError,
+  } = useQuery<WalletBalance, AxiosError<{ message: string }>>({
+    queryKey: ['walletBalance', walletId],
+    queryFn: async (): Promise<WalletBalance> => {
+      if (!walletId) {
+        throw new Error('Wallet ID not found');
+      }
+      const response = await WalletService.getWalletBalance(walletId);
+      return response.data;
+    },
+    enabled: !!walletId,
+    onError: (error: AxiosError<{ message: string }>) => {
+      console.error('Failed to fetch wallet balance:', error.message);
+      toast.error(`Error: ${error.response?.data.message || error.message}`);
+    },
+    onSuccess: (data: WalletBalance) => {
+      const { message, result } = data;
+      console.log('Wallet balance fetched successfully:', result);
+      toast.success(message);
+    },
+  });
 
   const toggleBalanceVisibility = () => {
     setIsBalanceVisible(!isBalanceVisible);
   };
 
-  const copyWalletId = () => {
-    navigator.clipboard.writeText(walletId);
-  };
+  if (getWalletQuery.isLoading || isBalanceLoading) {
+    return (
+      <div className="flex flex-col justify-center items-center">
+        <Image src="/Opener Loading.gif" alt="load" width={300} height={30} />;
+        <h1 className="font-bold text-xl">
+          We are getting your wallet details...
+        </h1>
+      </div>
+    );
+  }
+
+  if (getWalletQuery.isError || isBalanceError) {
+    return (
+      <div className="flex flex-col justify-center items-center">
+        <Image src="/Failed Mail.gif" alt="load" width={300} height={30} />;
+        <h1 className="font-bold text-xl">
+          Sorry we couldn't get your wallet details...
+        </h1>
+      </div>
+    );
+  }
 
   return (
     <>
@@ -55,10 +122,10 @@ const Wallet = () => {
             <CardFooter className="flex flex-col items-start px-4 md:px-6">
               <div className="flex items-center">
                 <h1 className="mr-1 font-semibold text-sm md:text-base">
-                  Account No:
+                  Account Number: {accountNumber}
                 </h1>
-                <h2 className="mr-2 text-sm md:text-base">{walletId}</h2>
-                <button onClick={copyWalletId} className="text-lg md:text-xl">
+                <h2 className="mr-2 text-sm md:text-base"></h2>
+                <button className="text-lg md:text-xl">
                   <FaCopy />
                 </button>
               </div>
@@ -74,7 +141,9 @@ const Wallet = () => {
                 </button>
               </div>
               <p className="font-bold text-xl md:text-2xl mt-2">
-                {isBalanceVisible ? '₦ 0.00' : '******'}
+                {isBalanceVisible
+                  ? `₦ ${balanceData?.result.availableBalance.toFixed(2)}`
+                  : '******'}
               </p>
             </CardFooter>
           </Card>
