b28bddcb570a36d6a3628d155694f70e1659411f
oluwafemi <oluwafemioluwatuyi8@gmail.com>
2024-09-17 15:39:07 +0100

    changed the chatModel color

--- p2p-loan-app/src/app/(platform)/(others)/chatModal.tsx
+++ p2p-loan-app/src/app/(platform)/(others)/chatModal.tsx
@@ -1,6 +1,144 @@
+// import React, { useState } from "react";
+// import { useChat } from "../../../hooks/useChatBot";
+// import { Send } from "lucide-react";
+// import { Avatar, AvatarFallback } from "../../../components/ui/avatar";
+// import { useAuthState } from "../../../store/authStore";
+// import { ChatHistoryItem } from "../../../services/chatService";
+
+// interface ChatModalProps {
+//   isOpen: boolean;
+//   onClose: () => void;
+// }
+
+// const ChatModal: React.FC<ChatModalProps> = ({ isOpen, onClose }) => {
+//   const [newMessage, setNewMessage] = useState("");
+//   const [chatHistory, setChatHistory] = useState<ChatHistoryItem[]>([]);
+//   const [isLoading, setIsLoading] = useState(false); // Loading state
+//   const { createChatMutation } = useChat();
+//   const { user } = useAuthState();
+
+//   const handleSendMessage = () => {
+//     if (newMessage.trim() !== "") {
+//       const userMessage = { role: "user", content: newMessage };
+
+//       // Update chat history with user's message
+//       setChatHistory((prev) => [...prev, userMessage]);
+
+//       setIsLoading(true);
+
+//       // Trigger chat mutation to send message
+//       createChatMutation.mutate(
+//         {
+//           newMessage,
+//           chatHistory: [...chatHistory],
+//         },
+//         {
+//           onSuccess: (data) => {
+//             console.log("Mutation Success:", data);
+//             const aiMessage: ChatHistoryItem = {
+//               role: "assistant",
+//               content: data,
+//             };
+//             setChatHistory((prev) => [...prev, aiMessage]);
+//             console.log("Updated Chat History:", chatHistory);
+//           },
+//           onError: (error) => {
+//             console.error("Error sending message:", error);
+
+//             // Set loading state to false on error
+//             setIsLoading(false);
+//           },
+//         }
+//       );
+
+//       // Clear the message input
+//       setNewMessage("");
+//     }
+//   };
+
+//   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+//     if (e.key === "Enter") {
+//       handleSendMessage();
+//     }
+//   };
+
+//   if (!isOpen) return null;
+
+//   return (
+//     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+//       <div className="bg-white p-6 rounded-md w-full max-w-3xl h-[80vh] flex flex-col">
+//         <div className="flex justify-between items-center mb-4">
+//           <h2 className="text-xl font-bold">BorrowHub</h2>
+//           <button onClick={onClose} className="text-red-500">
+//             Close
+//           </button>
+//         </div>
+
+//         <div className="chat-history overflow-y-auto flex-grow mb-4">
+//           {chatHistory.map((message, index) => (
+//             <div
+//               key={index}
+//               className={`mb-2 flex items-center ${
+//                 message.role === "user" ? "justify-end" : "justify-start"
+//               }`}
+//             >
+//               {message.role === "assistant" && (
+//                 <Avatar className="mr-2 bg-white p-1">
+//                   <AvatarFallback className="bg-sky-50 text-white">
+//                     A
+//                   </AvatarFallback>
+//                 </Avatar>
+//               )}
+
+//                {isLoading && (
+//                   <div className="flex justify-start mb-2">
+//                     <p className="p-2 rounded-md mx-2 bg-gray-200">......</p>
+//                   </div>
+//                 )}
+//               <p
+//                 className={`p-2 rounded-md mx-2 ${
+//                   message.role === "user" ? "bg-blue-100" : "bg-gray-200"
+//                 }`}
+//               >
+//                 {message.content}
+//               </p>
+//               {message.role === "user" && (
+//                 <Avatar className="ml-2 bg-white p-1">
+//                   <AvatarFallback className="bg-green-500 text-white">
+//                     {user?.firstName?.charAt(0).toUpperCase() || "U"}
+//                   </AvatarFallback>
+//                 </Avatar>
+//               )}
+//             </div>
+//           ))}
+//         </div>
+
+//         <div className="flex items-center">
+//           <input
+//             type="text"
+//             value={newMessage}
+//             onChange={(e) => setNewMessage(e.target.value)}
+//             onKeyDown={handleKeyDown}
+//             className="flex-grow p-2 border border-gray-300 rounded-md"
+//             placeholder="Type here to chat with me..."
+//           />
+//           <button
+//             onClick={handleSendMessage}
+//             className="ml-2 bg-sky-500 text-white p-2 rounded-md flex items-center justify-center"
+//           >
+//             <Send className="w-5 h-5" />
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ChatModal;
+
 import React, { useState } from "react";
 import { useChat } from "../../../hooks/useChatBot";
-import { Send } from "lucide-react";
+import { Send, SidebarClose } from "lucide-react";
 import { Avatar, AvatarFallback } from "../../../components/ui/avatar";
 import { useAuthState } from "../../../store/authStore";
 import { ChatHistoryItem } from "../../../services/chatService";
@@ -13,6 +151,7 @@ interface ChatModalProps {
 const ChatModal: React.FC<ChatModalProps> = ({ isOpen, onClose }) => {
   const [newMessage, setNewMessage] = useState("");
   const [chatHistory, setChatHistory] = useState<ChatHistoryItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false); // Loading state for assistant's response
   const { createChatMutation } = useChat();
   const { user } = useAuthState();
 
@@ -22,6 +161,9 @@ const ChatModal: React.FC<ChatModalProps> = ({ isOpen, onClose }) => {
 
       // Update chat history with user's message
       setChatHistory((prev) => [...prev, userMessage]);
+
+      // Set loading state to true to show assistant's loading message
+      setIsLoading(true);
 
       // Trigger chat mutation to send message
       createChatMutation.mutate(
@@ -37,10 +179,15 @@ const ChatModal: React.FC<ChatModalProps> = ({ isOpen, onClose }) => {
               content: data,
             };
             setChatHistory((prev) => [...prev, aiMessage]);
-            console.log("Updated Chat History:", chatHistory);
+
+            // Set loading state to false after receiving the assistant's response
+            setIsLoading(false);
           },
           onError: (error) => {
             console.error("Error sending message:", error);
+
+            // Set loading state to false in case of error
+            setIsLoading(false);
           },
         }
       );
@@ -59,12 +206,12 @@ const ChatModal: React.FC<ChatModalProps> = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+    <div  onClick={onClose} className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
       <div className="bg-white p-6 rounded-md w-full max-w-3xl h-[80vh] flex flex-col">
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-xl font-bold">BorrowHub</h2>
           <button onClick={onClose} className="text-red-500">
-            Close
+            <SidebarClose/>
           </button>
         </div>
 
@@ -72,14 +219,14 @@ const ChatModal: React.FC<ChatModalProps> = ({ isOpen, onClose }) => {
           {chatHistory.map((message, index) => (
             <div
               key={index}
-              className={`mb-2 flex items-center ${
+              className={`mb-7 flex items-start ${
                 message.role === "user" ? "justify-end" : "justify-start"
               }`}
             >
               {message.role === "assistant" && (
                 <Avatar className="mr-2 bg-white p-1">
-                  <AvatarFallback className="bg-sky-50 text-white">
-                    A
+                  <AvatarFallback className="bg-sky-500 text-white">
+                    BH
                   </AvatarFallback>
                 </Avatar>
               )}
@@ -91,14 +238,21 @@ const ChatModal: React.FC<ChatModalProps> = ({ isOpen, onClose }) => {
                 {message.content}
               </p>
               {message.role === "user" && (
-                <Avatar className="ml-2 bg-white p-1">
-                  <AvatarFallback className="bg-green-500 text-white">
+                <Avatar className="ml-2 bg- p-1">
+                  <AvatarFallback className="bg-sky-500 text-white">
                     {user?.firstName?.charAt(0).toUpperCase() || "U"}
                   </AvatarFallback>
                 </Avatar>
               )}
             </div>
           ))}
+
+          {/* Show loading dots only in assistant's message area */}
+          {isLoading && (
+            <div className="flex justify-start mb-2">
+              <p className="p-2 rounded-md mx-2 bg-gray-200">......</p>
+            </div>
+          )}
         </div>
 
         <div className="flex items-center">
